2ed6d456f25dd9a513caa9e1cbb2ae7e141e792c
YunYeoJin <duwls0349@naver.com>
2022-08-13 20:03:31 +0900

    Initial Commit

---
+++ OpenWeather/ViewController.swift
@@ -0,0 +1,19 @@
+//
+//  ViewController.swift
+//  OpenWeather
+//
+//  Created by 윤여진 on 2022/08/13.
+//
+
+import UIKit
+
+class ViewController: UIViewController {
+
+    override func viewDidLoad() {
+        super.viewDidLoad()
+        // Do any additional setup after loading the view.
+    }
+
+
+}
+
